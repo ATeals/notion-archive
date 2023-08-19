56374da9b4f0e4fc3ed1f7eb9976d3316d125638
ateals <ateals@icloud.com>
2023-08-19 18:58:02 +0900

    error?

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from "react";
 import ReviewList, { Skeleton } from "./_components/ReviewList";
-import { RetrieveBlockChildren } from "@/util/api/notion";
 
 export default () => {
     return (
