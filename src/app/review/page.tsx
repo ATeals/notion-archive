1f2cffaa92d9a3613f6dde5e42f58cbb50742817
ateals <ateals@icloud.com>
2023-08-19 18:55:33 +0900

    error

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -2,9 +2,7 @@ import { Suspense } from "react";
 import ReviewList, { Skeleton } from "./_components/ReviewList";
 import { RetrieveBlockChildren } from "@/util/api/notion";
 
-export default async () => {
-    const res = RetrieveBlockChildren("703b7e8f528a431ea00e8cfd9536f086");
-    console.log(res);
+export default () => {
     return (
         <section className="w-full m-2">
             <Suspense
