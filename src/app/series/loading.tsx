a456c5618d23a18b2abd9a6bc3ec001f97102b1d
ateals <ateals@icloud.com>
2023-08-19 21:24:07 +0900

    loader

--- src/app/series/loading.tsx
+++ src/app/series/loading.tsx
@@ -1,3 +1,11 @@
+import { Skeleton } from "./_components/PostList";
+
 export default () => {
-    return <>loading...</>;
+    return (
+        <section className="w-full">
+            <h1 className="m-5 text-2xl">All Post</h1>
+
+            <Skeleton />
+        </section>
+    );
 };
